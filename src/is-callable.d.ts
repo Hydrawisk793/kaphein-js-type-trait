a0aa0f2f97b68cb556e83977d524745d1737dcb5
Hydrawisk793 <hyw793@gmail.com>
2021-04-29 16:26:49 +0100

    docs(is-callable): improve the description.

--- src/is-callable.d.ts
+++ src/is-callable.d.ts
@@ -1,8 +1,8 @@
 /**
- *  Tests if the given value is callable.  
- *  This function tests if the given value has `call` and `apply` that are properties of `Function.prototype`.
+ *  Tests if the given value satisfies callable.  
+ *  Callable in this context means that a given value is a real function or has `call` and `apply` property that are properties of `Function.prototype`.
  *  @param v A value to test.
- *  @returns `true` if the value is callable, `false` otherwise.
+ *  @returns `true` if the value satisfies callable, `false` otherwise.
  */
 export declare function isCallable(
     v : any
